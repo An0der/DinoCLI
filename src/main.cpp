ad1032428032dcba1d9a3ec456f9c0cc8977b315
Khamzat Visingiriev <hamzat.visingirievich@gmail.com>
2025-08-21 23:16:34 +0300

    Added ncurses dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <ncurses.h>
 
 int main()
 {
     std::cout << "HEllo world" << std::endl;
-
+    initscr();			/* Start curses mode 		  */
+	printw("Hello World !!!");	/* Print Hello World		  */
+	refresh();			/* Print it on to the real screen */
+	getch();			/* Wait for user input */
+	endwin();	
+    
     return 0;
 }
